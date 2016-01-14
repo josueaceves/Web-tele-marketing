fbd917354805cac8d8641eb341f4ae127d550c09
josue aceves <josueaceves.ja@gmail.com>
2016-01-14 12:26:47 -0800

    add sessions and user authentication

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -5,7 +5,19 @@ class UsersController < ApplicationController
 	end
 
 	def create
-
+		@user = User.new(user_params)
+		if @user.save
+			redirect_to '/'
+		else
+			@errors = @user.errors.full_messages
+			render 'new'
+		end
 	end
-	
+
+
+	private 
+	def user_params
+    params.require(:user).permit(:first_name, :last_name, :email, :password)
+  end
+
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,6 @@
 Rails.application.routes.draw do
   
-  root 'sessions#new'
+  root 'landings#index'
 
   resources :users
 
