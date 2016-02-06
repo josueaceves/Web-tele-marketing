2f7e69015eeb581a8d0ba7b5006e3d33985282a2
josue aceves <josueaceves.ja@gmail.com>
2016-02-05 16:55:07 -0800

    add env file to gitignore

--- app/controllers/twilio_controller.rb
+++ app/controllers/twilio_controller.rb
@@ -3,13 +3,13 @@ class TwilioController < ApplicationController
 	def call
   	@account_sid = ENV['TWILIO_ACCOUNT_SID']
   	@auth_token = ENV['TWILIO_AUTH_TOKEN']
-  	@phone = params[:number]
+  	@phone = params[:phone]
   	@name = params[:name]
 	  # set up a client to talk to the Twilio REST API
 	  @client = Twilio::REST::Client.new(@account_sid, @auth_token)
 	   
 	  @call = @client.account.calls.create(
-	    :from => '+18056234397',   # From your Twilio number
+	    :from => '+18052609071',   # From your Twilio number
 	    :to => '+1' + @phone ,     # To any number
 	    # Fetch instructions from this URL when the call connects
 	    :url => 'http://twimlets.com/holdmusic?Bucket=com.twilio.music.ambient'
