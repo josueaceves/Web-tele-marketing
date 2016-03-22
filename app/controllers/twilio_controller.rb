612a64492b96a186b1e67a8f2c42f91fb862b865
josue aceves <josueaceves.ja@gmail.com>
2016-03-22 15:49:35 -0700

    debugging twilio

--- app/controllers/twilio_controller.rb
+++ app/controllers/twilio_controller.rb
@@ -46,7 +46,7 @@ class TwilioController < ApplicationController
     puts "params[:CallSid] below"
     p call_sid
     @client = Twilio::REST::Client.new(@@account_sid, @@auth_token)
-    @list = current_user.contact_lists.find_by(id: session[:last_contact_list_id])
+    @list = User.find_by(id: session[:user_id]).contact_lists.find_by(id: session[:last_contact_list_id])
     puts "list below"
     p @list
     case user_selection
