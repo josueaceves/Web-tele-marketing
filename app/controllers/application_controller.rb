f1284604931f4fa0f68eeb6f6620dd15af24ecc7
josue aceves <josueaceves.ja@gmail.com>
2016-03-28 14:56:09 -0700

    style lists indez

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -4,11 +4,17 @@ class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
   protect_from_forgery with: :null_session, if: Proc.new { |c| c.request.format == 'application/json' }
 
-  helper_method :current_user
+  helper_method :current_user, :decode_call_response
 
 
    def current_user
     # User.find_by(id: session[:user_id])
     @current_user ||=  User.find_by_id(session[:user_id])
   end
+
+  def decode_call_response(response_number)
+    response_hash = {"1" => "Le intereza, Favor de regresar la llamada", "2" => "Le Redirigio la llamada", "3" => "No le Intereza"}
+    response_hash[response_number]
+  end
+
 end
