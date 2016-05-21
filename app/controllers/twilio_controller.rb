7b4f219a974e24c2cd1ddb9868041f20592f0131
josue aceves <josueaceves.ja@gmail.com>
2016-05-21 16:27:42 -0700

    debug

--- app/controllers/twilio_controller.rb
+++ app/controllers/twilio_controller.rb
@@ -59,12 +59,12 @@ class TwilioController < ApplicationController
       contact.response = "1"
       contact.save
       # @output = "Uno de nuestros representatantes se comunicara con usted en seguida."
-      p "debugging***********************"
-      p "debugging***********************"
-      p "debugging***********************"
-      p "debugging***********************"
+      # p "debugging***********************"
+      # p "debugging***********************"
+      # p "debugging***********************"
+      # p "debugging***********************"
       p "+1" + params[:current_user_phone]
-      twiml_say(@output)
+      # twiml_say(@output)
       twiml_dial("+1" + params[:current_user_phone])
     when "2"
       contact.response = "2"
