3edbd181aef4e87f9c0d096e5e9da7bc8faefd46
josue aceves <josueaceves.ja@gmail.com>
2016-05-21 12:22:13 -0700

    debugging

--- app/controllers/contact_lists_controller.rb
+++ app/controllers/contact_lists_controller.rb
@@ -5,22 +5,23 @@ class ContactListsController < ApplicationController
 
     # TODO: reset code below when Montero subscription ends
     if current_user.email == "nuvilife.jose13@gmail.com" || current_user.number == "7145913108"
-      @@account_sid = ENV['TWILIO_MONTERO_ACCOUNT_SID']
-    	@@auth_token = ENV['TWILIO_MONTERO_AUTH_TOKEN']
+      @account_sid = ENV['TWILIO_MONTERO_ACCOUNT_SID']
+    	@auth_token = ENV['TWILIO_MONTERO_AUTH_TOKEN']
 
       p "*******************************"
       p "contact list controller"
       p "this  app is running with the right env variables"
-      p @@account_sid
-      p @@auth_token
+      p @account_sid
+      p @auth_token
       p "*******************************"
 
     elsif current_user.email == "josueaceves.ja@gmail.com"
-    	@@account_sid = ENV['TWILIO_ACCOUNT_SID']
-    	@@auth_token = ENV['TWILIO_AUTH_TOKEN']
+      p "using josues account"
+    	@account_sid = ENV['TWILIO_ACCOUNT_SID']
+    	@auth_token = ENV['TWILIO_AUTH_TOKEN']
     end
 
-    @client = Twilio::REST::Client.new(@@account_sid, @@auth_token)
+    @client = Twilio::REST::Client.new(@account_sid, @auth_token)
     @lists = current_user.contact_lists.all
   end
 
